99dd9a61765ffa78088453bd0d430a97cd4d7977
lidewang <lidewang@ky-tech.com.cn>
2019-10-27 19:32:20 +0800

    feat: 提交代码

--- react-admin/src/components/charts/Recharts.tsx
+++ react-admin/src/components/charts/Recharts.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import ReactEcharts from 'echarts-for-react';
-import { notification } from 'antd';
+import { notification, Card } from 'antd';
 import { getLotteryNum } from '../../axios'
+import {string} from "prop-types";
 
 const position = 0
 const ordNumArr = ['3', '4', '5', '6', '7']
-const option = {
+let option = {
     color: (params: any) => {
         return ordNumArr.includes(params.data) ? '#00FA9A' : '#FF4500'
     },
@@ -54,8 +55,11 @@ const option = {
     ],
 };
 let timeid: any
+let notIn: string[] = []
+let maxNotIn: string[] = []
 const EchartsBar = (): React.ReactElement => {
     let [options, setOptions] = useState(option);
+    let [title, setTitle] = useState({name: '', value: []});
     useEffect(() => {
         async function fetchData() {
             let res: Array<object> = await getLotteryNum({lotCode: 'TEQ28'})
@@ -68,48 +72,65 @@ const EchartsBar = (): React.ReactElement => {
                 let haoMao: string = num.haoMa.charAt(position)
                 if (!ordNumArr.includes(haoMao)) {
                     times++
+                    notIn.push(num.qiHao.slice(num.qiHao.length - 3, num.qiHao.length))
                 } else {
                     if (times > max) {
                         max = times
+                        maxNotIn = notIn
                     }
+                    notIn = []
                     times = 0
                 }
             })
-            notification.warning({
-                message: `总共连续没中${max}期`
-            })
-            setOptions(oldOptions => ({
+            setTitle((oldTitle: any) => ({
+                ...oldTitle,
+                name: `总共连续没中${max}期`,
+                value: maxNotIn
+            }))
+            setOptions((oldOptions: any) => ({
                 ...oldOptions,
-                xAxis : [
+                xAxis: [
                     {
-                        type : 'category',
-                        data : xData,
+                        type: 'category',
+                        data: xData,
                         axisTick: {
                             alignWithLabel: true
                         }
                     }
                 ],
-                series : [
+                series: [
                     {
-                        name: position === 0 ? '百位': position === 2 ? '十位' : '个位',
-                        type:'bar',
+                        name: position === 0 ? '百位' : position === 2 ? '十位' : '个位',
+                        type: 'bar',
                         data: yData,
                         label: {
                             show: true
                         }
                     },
                 ]
-            }))
+            }));
         }
         fetchData()
        timeid = setInterval(() => fetchData(), 60000)
         return ( ) => clearInterval(timeid)
     }, []);
 
-    return <ReactEcharts
-    option={options}
-    style={{height: '100%', width: '100%'}}
-    className={'react_for_echarts'}
-    />
+    return (
+        <div>
+            <Card title={title.name}>
+                title.value.map(item => {
+                    <span>{ item }</span>
+                  }
+                )
+            </Card>
+          <div>
+              <ReactEcharts
+                  option={options}
+                  style={{height: '400px', width: '100%'}}
+                  className={'react_for_echarts'}
+              />
+          </div>
+        </div>
+    )
 };
 export default EchartsBar
